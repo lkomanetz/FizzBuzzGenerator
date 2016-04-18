9ab096e9581bcaf9603a4d61865ae2ed3be75f2f
Logan Komanetz <logan.komanetz@gmail.com>
2016-04-18 12:26:58 -0500

    Creates the Noridian application for FizzBuzz.

---
+++ NoridianTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzLibrary;
+
+namespace NoridianTest {
+	public class Program {
+
+		static void Main(string[] args) {
+			IFizzBuzzGenerator generator = new NoridianGenerator(1, 100);
+			Console.Write(generator.Generate());
+			Console.ReadLine();
+		}
+
+	}
+
+}
